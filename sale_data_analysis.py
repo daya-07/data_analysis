1c3b9a9bd1923875f75b0f54a97701941d563448
daya-07 <dayaedp07@gmail.com>
2026-04-20 12:02:00 +0530

    Updated sale_data_analysis.py

--- sale_data_analysis.py
+++ sale_data_analysis.py
@@ -31,16 +31,20 @@ sum_arr.append(np.sum(timothy[:, -1]))
 
 # Managers vs Sale amt #
 plt.pie(x=sum_arr, labels=managers)
+plt.title("Managers vs Sale amt")
 plt.show()
 
 # Sale amt vs Region #
 plt.bar(arr[:, 1], arr[:, -1])
+plt.title("Sale amt vs Region")
 plt.show()
 
 # Sale amt vs Salesman #
 plt.bar(arr[:, 3], arr[:, -1], color="lightgreen")
+plt.title("Sale amt vs Salesman")
 plt.show()
 
-#Sale amt vs Item #
+# Sale amt vs Item #
 plt.bar(arr[:, 4], arr[:, -1], color="lightblue")
+plt.title("Sale amt vs Item")
 plt.show()
